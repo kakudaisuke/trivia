01d682acfffeb36b134e0607390e41d26cb6ca59
kakudaisuke <dk.15step@gmail.com>
2023-01-21 17:55:59 +0900

    とりあえずボタンとカウンターを表示する

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="hee-btn-component">
+        <img src="hee_btn.jpg" alt="へえボタン"></img>
+        <audio src="hee.mp3" />
+        <div><span>0</span>へえ</div>
+      </div>
     </div>
   );
 }
