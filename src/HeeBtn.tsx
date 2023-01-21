10eac5c2bccf7c46532564f48aa0f4b18b0cf104
kakudaisuke <dk.15step@gmail.com>
2023-01-21 18:07:29 +0900

    ボタンをコンポーネント化

---
+++ src/HeeBtn.tsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+const HeeBtn = () => {
+  return (
+    <>
+      <div id="hee-btn-component">
+        <img src="hee_btn.jpg" alt="へえボタン"></img>
+        <audio src="hee.mp3" />
+        < div><span><strong>0</strong></span>へえ</div>
+      </div>
+    </>
+  )
+}
+
+export default HeeBtn;
