8529d367afff919bf46ad409cf0841ebe83feab5
kakudaisuke <dk.15step@gmail.com>
2023-01-22 16:04:14 +0900

    20までカウントできるようにする

--- src/HeeBtn.tsx
+++ src/HeeBtn.tsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
+
 const HeeBtn = () => {
+  const [count, setCount] = useState(0);
+
+  const addCount = () => {
+    if (count < 20) setCount(count + 1);
+  }
+
   return (
     <>
-      <div id="hee-btn-component">
+      <div id="hee-btn-component" onClick={addCount}>
         <img src="hee_btn.jpg" alt="へえボタン"></img>
-        <audio src="hee.mp3" />
-        < div><span><strong>0</strong></span>へえ</div>
+        <audio id="hee-audio" src="hee.mp3"></audio>
       </div>
+      < div><span><strong>{count}</strong></span>へえ</div>
     </>
   )
 }
