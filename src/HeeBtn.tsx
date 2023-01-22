bb4ae765dbbcc2276af0ac4340b77b63e75b4165
kakudaisuke <dk.15step@gmail.com>
2023-01-22 16:29:15 +0900

    カウンターをリセットできるように

--- src/HeeBtn.tsx
+++ src/HeeBtn.tsx
@@ -1,11 +1,24 @@
+import { useState, useEffect } from 'react';
+
 const HeeBtn = () => {
+  const [count, setCount] = useState(0);
+
+  const addCount = () => {
+    if (count < 20) setCount(count + 1);
+  }
+
+  const resetCount = () => {
+    setCount(0);
+  }
+
   return (
     <>
-      <div id="hee-btn-component">
+      <div id="hee-btn-component" onClick={addCount}>
         <img src="hee_btn.jpg" alt="へえボタン"></img>
-        <audio src="hee.mp3" />
-        < div><span><strong>0</strong></span>へえ</div>
+        <audio id="hee-audio" src="hee.mp3"></audio>
       </div>
+      < div><span><strong>{count}</strong></span>へえ</div>
+      <button onClick={resetCount}>リセット</button>
     </>
   )
 }
